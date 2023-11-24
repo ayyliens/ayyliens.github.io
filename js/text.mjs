80092b659bee9900f030a0393e34a6cf8bcca3e4
Yury Egorenkov <yury.egorenkov@gmail.com>
2023-11-24 10:19:01 +0100

    update texts

--- js/text.mjs
+++ js/text.mjs
@@ -6,7 +6,9 @@ We would like to use GPT4 abilities to write complex systems not only small piec
 export const intro = `
 # aliens.dev
 
-We are excited to announce that we are working on explore GPT-4 to build complete software systems, websites and mobile apps, not just small code snippets.
+GitHub [https://github.com/ayyliens](https://github.com/ayyliens)
+
+We build environment for GPT-4 to build complete software systems, websites and mobile apps, not just small code snippets.
 
 To accomplish this, we will develop prompts to guide GPT-4 through the entire software development lifecycle, including:
 
@@ -16,15 +18,15 @@ To accomplish this, we will develop prompts to guide GPT-4 through the entire so
 - Deployment
 - Ongoing maintenance and support
 
-We will also build feedback loops and automatic correction systems to enable GPT-4 to improve its own work over time based on feedback. Our goal is for GPT-4 to be able to develop and maintain complex software systems with limited human involvement. We believe this could significantly accelerate software development and allow humans to focus on more creative and strategic work.
+We will build feedback loops, multi-step, many-attempt, context based, self-reflection GPT conversation and automatic correction systems to enable GPT-4 improve its own work over time based on feedback. 
+
+Our goal is for GPT-4 to be able to develop and maintain complex software systems with limited human involvement. It could significantly accelerate software development and allow humans to focus on more creative and strategic work.
 
 We will keep the community updated on our progress and hope to share examples of systems developed by GPT-4 in the coming months. Please share any feedback or suggestions you may have. We aim to build GPT-4's capabilities with the help of the community.
 
 We cordially invite you to join us on our journey. We would greatly appreciate any assistance in discussing ideas, making attempts, writing code, or providing financial support.
 
 If you wish to stay connected and be updated on our progress, you can subscribe.
-
-Check our GitHub repository [https://github.com/ayyliens](https://github.com/ayyliens)
 
 Join Discord channel
 `
@@ -55,8 +57,4 @@ Even if you just show interest in our work and observe, this is already a help! 
 If you can get involved, share ideas, do something of value, welcome, we'd love it!
 
 If you can code, pull requests, find and share how to write quality code with GPT4 and other AIs, great!
-
-If you can provide us with all possible financial support, we will be eternally grateful. At the moment we are employed and can only work on this project part of the time.
-
-We also really need any help to build a community.
 `
